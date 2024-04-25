73b0c823e4af0453abbe96e611bf1985d9745528
jpedro453 <jpedroswc453@gmail.com>
2024-04-25 15:42:22 -0300

    test: ensure sign up controller throw if password and password confirmation are not equal

--- src/tests/presentation/controllers/auth/signup-controller.spec.ts
+++ src/tests/presentation/controllers/auth/signup-controller.spec.ts
@@ -1,4 +1,5 @@
 import { ISignUp } from '@/interfaces/domain/useCases/auth/SignUp'
+import { badRequest } from '@/interfaces/presentation/codes'
 import { SignUpController } from '@/presentation/controllers/auth/SignUpController'
 import { makeSignUpUseCaseStub } from '@/tests/mocks/useCases/SignUp.usecase.mock'
 
@@ -25,5 +26,19 @@ describe('SignUp controller', () => {
             }
         })
         expect(res.statusCode).toBe(400)
+    })
+    test('Should return 400 if password != password_confirmation', async () => {
+        const { sut } = makeSut()
+
+        const res = await sut.handle({
+            body: {
+                name: 'any',
+                email: 'any',
+                password: 'pass',
+                password_confirmation: 'any'
+            }
+        })
+        expect(res.statusCode).toBe(400)
+        expect(res).toEqual(badRequest(new Error('As senhas não são iguais')))
     })
 })

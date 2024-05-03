307541b76a7c3b3b8441e7985a18b9b71b31ca88
jpedro453 <jpedroswc453@gmail.com>
2024-05-03 20:42:16 -0300

    test: ensure curretUserInfoController returns status 400 if no user found

---
+++ src/presentation/controllers/auth/CurrentUserInfo.ts
@@ -0,0 +1,24 @@
+import { IGetByToken } from '@/interfaces/domain/useCases/auth/GetByToken'
+import { badRequest, ok, serverError } from '@/interfaces/presentation/codes'
+import { IController } from '@/interfaces/presentation/controller'
+import { IHttpRequest, IHttpResponse } from '@/interfaces/presentation/http'
+
+export class CurrentUserInfo implements IController {
+    constructor(private readonly getByToken: IGetByToken) {}
+
+    async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+        try {
+            const { access_token } = httpRequest.headers
+
+            const user = await this.getByToken.get(access_token)
+
+            if (!user) {
+                return badRequest(new Error('Não foi encontrado nenhum usuário com esse token.'))
+            }
+
+            return ok(user)
+        } catch (error) {
+            return serverError(error)
+        }
+    }
+}
---
+++ src/tests/presentation/controllers/auth/get-user-info-controller.spec.ts
@@ -0,0 +1,27 @@
+import { IGetByToken } from '@/interfaces/domain/useCases/auth/GetByToken'
+import { CurrentUserInfo } from '@/presentation/controllers/auth/CurrentUserInfo'
+import { makeFakeGetByToken } from '@/tests/mocks/useCases/GetByTokenUseCase.mock'
+
+interface ISut {
+    getByToken: IGetByToken
+    sut: CurrentUserInfo
+}
+
+const makeSut = (): ISut => {
+    const getByToken = makeFakeGetByToken()
+    const sut = new CurrentUserInfo(getByToken)
+
+    return { sut, getByToken }
+}
+
+describe('GetUserInfoController', () => {
+    test('Should return 400 if no user found', async () => {
+        const { sut, getByToken } = makeSut()
+
+        jest.spyOn(getByToken, 'get').mockReturnValueOnce(Promise.resolve(null))
+
+        const res = await sut.handle({ headers: 'any_token' })
+
+        expect(res.body).toBeTruthy()
+    })
+})

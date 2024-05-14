8527a880f10f1657e9f88eb07ab9fa80dca7f9df
jpedro453 <jpedroswc453@gmail.com>
2024-05-14 15:05:54 -0300

    feat: delete questions category controller

---
+++ src/presentation/controllers/questionsCategory/DeleteQuestionsCategoryController.ts
@@ -0,0 +1,24 @@
+import { IDeleteQuestionsCategory } from '@/interfaces/domain/useCases/questionsCategory/DeleteQuestionsCategory'
+import { badRequest, ok } from '@/interfaces/presentation/codes'
+import { IController } from '@/interfaces/presentation/controller'
+import { IHttpRequest, IHttpResponse } from '@/interfaces/presentation/http'
+import { mapErrorToHttpResponse } from '@/presentation/helpers/ErrorMapper'
+
+export class DeleteQuestionsCategoryController implements IController {
+    constructor(private readonly deleteCategory: IDeleteQuestionsCategory) {}
+    async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+        try {
+            const { id } = httpRequest.query
+
+            if (!id) {
+                return badRequest(new Error('ID da categoria é obrigatório.'))
+            }
+
+            const deleted = await this.deleteCategory.delete(id)
+
+            return ok(deleted)
+        } catch (error) {
+            return mapErrorToHttpResponse(error)
+        }
+    }
+}
---
+++ src/tests/presentation/controllers/questionsCategory/delete-questions-category-controller.spec.ts
@@ -0,0 +1,48 @@
+import { IDeleteQuestionsCategory } from '@/interfaces/domain/useCases/questionsCategory/DeleteQuestionsCategory'
+import { DeleteQuestionsCategoryController } from '@/presentation/controllers/questionsCategory/DeleteQuestionsCategoryController'
+import { NotFoundError } from '@/utils/customErrors'
+
+const makeSut = () => {
+    class DeleteCategory implements IDeleteQuestionsCategory {
+        async delete(id: string): Promise<boolean> {
+            return Promise.resolve(true)
+        }
+    }
+    const deleteCategory = new DeleteCategory()
+    const sut = new DeleteQuestionsCategoryController(deleteCategory)
+    return { sut, deleteCategory }
+}
+
+describe('DeleteQuestionsCategoryController', () => {
+    test('should return 400 if no id provided', async () => {
+        const { sut } = makeSut()
+        const res = await sut.handle({ query: { id: null } })
+
+        expect(res.statusCode).toBe(400)
+    })
+    test('should return 400 if no category found', async () => {
+        const { sut, deleteCategory } = makeSut()
+
+        jest.spyOn(deleteCategory, 'delete').mockReturnValueOnce(
+            Promise.reject(new NotFoundError('Categoria não encontrada'))
+        )
+        const res = await sut.handle({ query: { id: '123dawda' } })
+
+        expect(res.statusCode).toBe(400)
+    })
+    test('should return 500 if server error', async () => {
+        const { sut, deleteCategory } = makeSut()
+
+        jest.spyOn(deleteCategory, 'delete').mockReturnValueOnce(Promise.reject(new Error('any error')))
+        const res = await sut.handle({ query: { id: '123dawda' } })
+
+        expect(res.statusCode).toBe(500)
+    })
+    test('should return 200 on delete success', async () => {
+        const { sut } = makeSut()
+
+        const res = await sut.handle({ query: { id: '123dawda' } })
+
+        expect(res.statusCode).toBe(200)
+    })
+})
